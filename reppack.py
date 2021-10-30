66ef3154c8d472dd422b4fb80847faba2fb22911
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-10-30 12:53:28 -0900

    Update reppack.py

--- reppack.py
+++ reppack.py
@@ -1,7 +1,9 @@
 import os
 import platform
-
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
 if platform.system() == "Windows":
     raise OSError("Don't run on Windows!")
 if not os.path.exists(os.path.expanduser("~") + "/Ccode"):
     raise OSError("Ccode language is not installed!")
+if not os.path.exists("pack-list"):
+    os.system("git clone -q https://github.com/Ccode-lang/pack-list")
